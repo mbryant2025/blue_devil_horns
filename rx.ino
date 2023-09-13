6c856fb0a2c7fd25db5b83d4e21b93daa2f45cff
Michael Bryant <mcb115@duke.edu>
2023-09-12 23:28:53 -0400

    fixed arduino code

--- rx.ino
+++ rx.ino
@@ -5,11 +5,11 @@ char ssid[] = "NEVER GONNA GIVE YOU UP 2.4";
 char pass[] = "60029454";
 int status = WL_IDLE_STATUS;
 WiFiServer server(80); // Create a server on port 80
-char status = "0";
+int toggle_blink = 0;
+double last_blink_check = millis();
+bool led1 = false;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
 
   pinMode(2, OUTPUT);
   digitalWrite(2, LOW);
@@ -21,15 +21,10 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);
 
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
-    Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
     delay(10000);
   }
-
-  Serial.println("You're connected to the network");
-  Serial.println("---------------------------------------");
-
+  
   digitalWrite(LED_BUILTIN, LOW);
 
   server.begin(); // Start the server
@@ -39,44 +34,52 @@ void loop() {
   WiFiClient client = server.available(); // Check if a client has connected
 
   if (client) {
-    Serial.println("New client connected");
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
+
+        toggle_blink = 0;
 
         if (c == '4') {
-          digitalWrite(2, HIGH);
-          delay(1000);
-          digitalWrite(3, LOW);
-          delay(1000);
-          status = '4';
+          toggle_blink = 1;
         } else if (c == '3') {
           digitalWrite(2, HIGH);
           digitalWrite(3, LOW);
-          status = '3';
         } else if (c == '2') {
           digitalWrite(2, LOW);
           digitalWrite(3, HIGH);
-          status = '2';
         } else if (c == '1') {
           digitalWrite(2, HIGH);
           digitalWrite(3, HIGH);
-          status = '1';
         } else if (c == '0') {
           digitalWrite(2, LOW);
           digitalWrite(3, LOW);
-          status = '0';
         }
       }
+
     }
     client.stop();
-    Serial.println("Client disconnected");
   }
-  if (status == '4' ){
-    digitalWrite(2, HIGH);
-    delay(1000);
-    digitalWrite(3, LOW);
-    delay(1000);
+  //blink
+  if (toggle_blink) {
+
+    // Once a second
+    if (millis() - last_blink_check > 1000) {
+
+      last_blink_check = millis();
+
+      if (led1) {
+        digitalWrite(2, HIGH);
+        digitalWrite(3, LOW);
+      } else {
+        digitalWrite(2, LOW);
+        digitalWrite(3, HIGH);
+      }
+
+      led1 = !led1;
+
+    }
+
+
   }
 }
